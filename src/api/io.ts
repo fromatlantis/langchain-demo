d8f1b8cfabb417e94eba313ce75ff79ed3946d4a
niuzhiwei3 <niuzhiwei3@jd.com>
2023-11-02 20:05:32 +0800

    test: res test

--- src/api/io.ts
+++ src/api/io.ts
@@ -11,56 +11,56 @@ import type { APIRoute } from 'astro';
 
 import { OPENAI_API_KEY } from '~/config';
 
-export const post: APIRoute = async ({ params, request }) => {
-  const outputParser = StructuredOutputParser.fromZodSchema(
-    z
-      .array(
-        z.object({
-          fields: z.object({
-            Name: z.string().describe("The name of the country"),
-            Capital: z.string().describe("The country's capital"),
-          }),
-        })
-      )
-      .describe("An array of Airtable records, each representing a country")
-  );
+// export const post: APIRoute = async ({ params, request }) => {
+//   const outputParser = StructuredOutputParser.fromZodSchema(
+//     z
+//       .array(
+//         z.object({
+//           fields: z.object({
+//             Name: z.string().describe("The name of the country"),
+//             Capital: z.string().describe("The country's capital"),
+//           }),
+//         })
+//       )
+//       .describe("An array of Airtable records, each representing a country")
+//   );
   
-  const chatModel = new ChatOpenAI({
-    openAIApiKey: OPENAI_API_KEY,
-    modelName: "gpt-3.5-turbo", // Or gpt-3.5-turbo
-    temperature: 0, // For best results with the output fixing parser
-  });
+//   const chatModel = new ChatOpenAI({
+//     openAIApiKey: OPENAI_API_KEY,
+//     modelName: "gpt-3.5-turbo", // Or gpt-3.5-turbo
+//     temperature: 0, // For best results with the output fixing parser
+//   });
   
-  const outputFixingParser = OutputFixingParser.fromLLM(chatModel, outputParser);
+//   const outputFixingParser = OutputFixingParser.fromLLM(chatModel, outputParser);
   
-  // Don't forget to include formatting instructions in the prompt!
-  const prompt = new PromptTemplate({
-    template: `Answer the user's question as best you can:\n{format_instructions}\n{query}`,
-    inputVariables: ["query"],
-    partialVariables: {
-      format_instructions: outputFixingParser.getFormatInstructions(),
-    },
-  });
+//   // Don't forget to include formatting instructions in the prompt!
+//   const prompt = new PromptTemplate({
+//     template: `Answer the user's question as best you can:\n{format_instructions}\n{query}`,
+//     inputVariables: ["query"],
+//     partialVariables: {
+//       format_instructions: outputFixingParser.getFormatInstructions(),
+//     },
+//   });
   
-  const answerFormattingChain = new LLMChain({
-    llm: chatModel,
-    prompt,
-    outputKey: "records", // For readability - otherwise the chain output will default to a property named "text"
-    outputParser: outputFixingParser,
-  });
+//   const answerFormattingChain = new LLMChain({
+//     llm: chatModel,
+//     prompt,
+//     outputKey: "records", // For readability - otherwise the chain output will default to a property named "text"
+//     outputParser: outputFixingParser,
+//   });
   
-  const result = await answerFormattingChain.call({
-    query: "List 5 countries.",
-  });
-  return new Response(JSON.stringify({
-    name: "nzw"
-  }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-};
+//   const result = await answerFormattingChain.call({
+//     query: "List 5 countries.",
+//   });
+//   return new Response(JSON.stringify({
+//     name: "nzw"
+//   }), {
+//     status: 200,
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   });
+// };
 
 export const get:APIRoute = async ({params, request}) =>{
   return new Response(

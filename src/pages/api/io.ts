9d154700b968c8709922adb69d14cc19629f6842
niuzhiwei3 <niuzhiwei3@jd.com>
2023-11-06 18:22:12 +0800

    test: country population

--- src/pages/api/io.ts
+++ src/pages/api/io.ts
@@ -16,7 +16,7 @@ export const post: APIRoute = async ({ params, request }) => {
                 z.object({
                     Name: z.string().describe('The name of the country'),
                     Capital: z.string().describe("The country's capital"),
-                    Number: z.number().describe("The country's peoples"),
+                    Number: z.number().describe("The country's population"),
                 }),
             )
             .describe('An array of Airtable records, each representing a country'),

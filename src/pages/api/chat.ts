b6521baf75f99e9645ca5c5e5e29e7c6ef938661
niuzhiwei3 <niuzhiwei3@jd.com>
2023-11-08 16:07:46 +0800

    feat: chat

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,4 +1,4 @@
-import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { OpenAI } from 'langchain/llms/openai';
 
 import type { APIRoute } from 'astro';
 
@@ -7,14 +7,14 @@ import { OPENAI_API_KEY } from '~/config';
 export const post: APIRoute = async ({ params, request }) => {
     const body = await request.json();
 
-    const model = new ChatOpenAI({
+    const model = new OpenAI({
         openAIApiKey: OPENAI_API_KEY,
         modelName: 'gpt-3.5-turbo', // Or gpt-3.5-turbo
         temperature: 0, // For best results with the output fixing parser
     });
 
     const result = await model.call(body.prompt);
-    return new Response(JSON.stringify(result), {
+    return new Response(result, {
         status: 200,
         headers: {
             'Content-Type': 'application/json',

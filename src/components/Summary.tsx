804d80df6a937931a6bfc4d9395c1339aa2b527f
niuzhiwei3 <niuzhiwei3@jd.com>
2023-11-15 10:59:52 +0800

    feat: summarization txt3

---
+++ src/components/Summary.tsx
@@ -0,0 +1,46 @@
+import { createSignal, For, Show } from 'solid-js';
+import LoadingMask from './LoadingMask';
+import text from '~/assets/state_of_the_union_zh.txt';
+
+const Summary = () => {
+    const [answer, setAnswer] = createSignal('');
+    const [loading, setLoading] = createSignal(false);
+
+    const handleSend = () => {
+        getAnswer();
+    };
+
+    const getAnswer = async () => {
+        setLoading(true);
+        const response = await fetch(`/api/summary`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'custom/non-standard',
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                prompt: prompt(),
+            }),
+        });
+        const result = await response.json();
+        setAnswer(result?.text);
+        setLoading(false);
+    };
+
+    return (
+        <div class="w-full flex-1 relative">
+            <Show when={loading()}>
+                <LoadingMask />
+            </Show>
+            <div class="w-full flex m-b-6">
+                <a href={text}></a>
+                <ar-button onClick={handleSend}>
+                    <ar-icon name="send"></ar-icon>
+                </ar-button>
+            </div>
+            {answer()}
+        </div>
+    );
+};
+
+export default Summary;

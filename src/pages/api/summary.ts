972f556835060c1f1164c05b9a874980fa36c93b
niuzhiwei3 <niuzhiwei3@jd.com>
2023-11-15 17:40:22 +0800

    feat: txt

--- src/pages/api/summary.ts
+++ src/pages/api/summary.ts
@@ -1,5 +1,6 @@
 import { OpenAI } from 'langchain/llms/openai';
 import { loadSummarizationChain, AnalyzeDocumentChain } from 'langchain/chains';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio';
 
 import type { APIRoute } from 'astro';
@@ -7,20 +8,25 @@ import type { APIRoute } from 'astro';
 import { OPENAI_API_KEY } from '~/config';
 
 export const post: APIRoute = async ({ params, request }) => {
-    const loader = new CheerioWebBaseLoader("https://mp.weixin.qq.com/s/cI8iTflC3YO_g5ZIIqP2YQ");
+    const loader = new CheerioWebBaseLoader('https://mp.weixin.qq.com/s/cI8iTflC3YO_g5ZIIqP2YQ');
     const docs = await loader.load();
-    console.log(docs)
+    // 文档分割
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    });
+    const splitDocs = await textSplitter.splitDocuments(docs);
     const model = new OpenAI({
         openAIApiKey: OPENAI_API_KEY,
         modelName: 'gpt-3.5-turbo', // Or gpt-3.5-turbo
         temperature: 0, // For best results with the output fixing parser
     });
-    const combineDocsChain = loadSummarizationChain(model);
-    const chain = new AnalyzeDocumentChain({
-        combineDocumentsChain: combineDocsChain,
-    });
+    const chain = loadSummarizationChain(model);
+    // const chain = new AnalyzeDocumentChain({
+    //     combineDocumentsChain: combineDocsChain,
+    // });
     const result = await chain.call({
-        input_document: docs,
+        input_document: splitDocs,
     });
     return new Response(
         JSON.stringify({

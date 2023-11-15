b7c3d64ee427f49a1488f96216691b4610b18461
niuzhiwei3 <niuzhiwei3@jd.com>
2023-11-15 14:29:14 +0800

    feat: txt

--- src/pages/api/summary.ts
+++ src/pages/api/summary.ts
@@ -5,13 +5,10 @@ import type { APIRoute } from 'astro';
 
 import { OPENAI_API_KEY } from '~/config';
 
-const url = new URL('../../../state_of_the_union_zh.txt', import.meta.url);
-
-
 export const post: APIRoute = async ({ params, request }) => {
     const body = await request.json();
     const decoder = new TextDecoder("utf-8");
-    const text = decoder.decode(await Deno.readFile(url));
+    const text = decoder.decode(await Deno.readFile('/state_of_the_union_zh.txt'));
     const model = new OpenAI({
         openAIApiKey: OPENAI_API_KEY,
         modelName: 'gpt-3.5-turbo', // Or gpt-3.5-turbo

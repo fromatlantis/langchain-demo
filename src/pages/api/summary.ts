c4abd82c7c67584538154d24a9c4e32d19e7757a
niuzhiwei3 <niuzhiwei3@jd.com>
2023-11-15 16:21:22 +0800

    feat: txt

--- src/pages/api/summary.ts
+++ src/pages/api/summary.ts
@@ -10,6 +10,7 @@ export const post: APIRoute = async ({ params, request }) => {
     const decoder = new TextDecoder('utf-8');
     const res = await fetch('https://langchain.deno.dev/state_of_the_union_zh.txt');
     const text = await res.text();
+    console.log(text)
     const model = new OpenAI({
         openAIApiKey: OPENAI_API_KEY,
         modelName: 'gpt-3.5-turbo', // Or gpt-3.5-turbo

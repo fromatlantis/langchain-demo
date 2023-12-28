a12d1a2e79a3427cd1d3ac443ebfefd5a72c3784
niuzhiwei3 <niuzhiwei3@jd.com>
2023-12-28 20:14:07 +0800

    feat: saler

--- src/pages/api/saler.ts
+++ src/pages/api/saler.ts
@@ -1,17 +1,18 @@
 import type { APIRoute } from 'astro';
 import { OpenAI } from 'langchain/llms/openai';
-// import { llm } from '~/gpts/llm/openai';
+import { llm } from '~/gpts/llm/openai';
 import { loadStageAnalyzerChain } from '~/gpts/sales-gpt/chains';
 
 export const POST: APIRoute = async ({ params, request }) => {
     try {
         const body = await request.json();
-        const llm = new OpenAI({
-          openAIApiKey: body.localKey,
-          modelName: 'gpt-3.5-turbo', // Or gpt-3.5-turbo
-          // streaming: true,
-          temperature: 0, // For best results with the output fixing parser
-      });
+        llm.openAIApiKey = body.localKey
+        // const llm = new OpenAI({
+        //   openAIApiKey: body.localKey,
+        //   modelName: 'gpt-3.5-turbo', // Or gpt-3.5-turbo
+        //   // streaming: true,
+        //   temperature: 0, // For best results with the output fixing parser
+        // });
         const stage_analyzer_chain = loadStageAnalyzerChain(llm, true);
         const res = await stage_analyzer_chain.call({ conversation_history: "", conversation_stage_id: 0 });
         console.log(res)

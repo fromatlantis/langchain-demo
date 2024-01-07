8e564d7076cca30dbf6288640457e86dc428e0fc
niuzhiwei3 <niuzhiwei3@jd.com>
2024-01-07 11:54:50 +0800

    test: chain prompt

--- src/gpts/saler/agent.ts
+++ src/gpts/saler/agent.ts
@@ -1,5 +1,5 @@
 import { RunnableSequence } from '@langchain/core/runnables';
-import { ChatPromptTemplate, MessagesPlaceholder } from '@langchain/core/prompts';
+import { ChatPromptTemplate, MessagesPlaceholder, PromptTemplate } from '@langchain/core/prompts';
 import { AgentExecutor, type AgentStep } from 'langchain/agents';
 import { ChatOpenAI } from '@langchain/openai';
 import { formatToOpenAIFunction } from 'langchain/tools';
@@ -9,6 +9,7 @@ import { formatToOpenAIFunctionMessages } from 'langchain/agents/format_scratchp
 import { OpenAIFunctionsAgentOutputParser } from 'langchain/agents/openai/output_parser';
 
 import { productSearch } from './tools';
+import { SALES_AGENT_INCEPTION_PROMPT } from './prompts';
 
 export const genExecutor = async (openAIApiKey: string) => {
     const model = new ChatOpenAI({
@@ -25,8 +26,21 @@ export const genExecutor = async (openAIApiKey: string) => {
         functions: tools.map((tool) => formatToOpenAIFunction(tool) as any),
     });
 
+    const SYSTEM = await PromptTemplate.fromTemplate(SALES_AGENT_INCEPTION_PROMPT).format({
+        salesperson_name: '小张',
+        salesperson_role: '销售经理',
+        company_name: 'Sleep Haven',
+        company_business:
+            'Sleep Haven是一家优质床垫公司，为客户提供最舒适、最支持的睡眠体验。我们提供一系列高品质的床垫、枕头和床上用品，旨在满足客户的独特需求。',
+        company_values:
+            'Sleep Haven的使命是为人们提供最好的睡眠解决方案，帮助他们获得更好的睡眠。我们相信高质量的睡眠对整体健康和幸福至关重要，我们致力于通过提供卓越的产品和客户服务来帮助我们的客户实现最佳睡眠。',
+        conversation_purpose: '了解他们是否希望通过购买优质床垫来获得更好的睡眠。',
+        conversation_history: '',
+        conversation_type: 'call',
+    });
+
     const prompt = ChatPromptTemplate.fromMessages([
-        ['system', "You are very powerful assistant, but don't know current events"],
+        ['system', SYSTEM],
         ['human', '{input}'],
         new MessagesPlaceholder('agent_scratchpad'),
     ]);

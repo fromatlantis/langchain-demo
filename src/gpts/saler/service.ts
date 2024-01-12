5e1861a5d2af2bcbb80dfd8bbd84e2327e5f7b94
niuzhiwei3 <niuzhiwei3@jd.com>
2024-01-12 16:44:27 +0800

    test: saler streamLog

--- src/gpts/saler/service.ts
+++ src/gpts/saler/service.ts
@@ -39,7 +39,7 @@ export class Service {
                             ) {
                                 const uint8Array = encoder.encode(addOp.value);
                                 controller.enqueue(uint8Array);
-                            } else if (addOp.path.startsWith('/streamed_output')) {
+                            } else if (addOp.path === '/final_output') {
                                 this.chatHistory.push(new AIMessage(addOp.value.output));
                             }
                         }

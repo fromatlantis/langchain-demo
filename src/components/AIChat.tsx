d9ed559cbf2c0d317fb03e623a83ced0ee71c701
niuzhiwei3 <niuzhiwei3@jd.com>
2024-01-12 11:26:04 +0800

    test: answer

--- src/components/AIChat.tsx
+++ src/components/AIChat.tsx
@@ -31,30 +31,31 @@ const AIChat = () => {
         if (!data) {
             throw new Error('没有返回数据，请稍后再试');
         }
-        console.log(data);
-        const reader = data.getIterator();
-        for await (const chunk of reader) {
-            // 解析数据块
-            console.log(JSON.stringify(chunk, null, 2));
-        }
-        // const reader = data.getReader();
-        // const decoder = new TextDecoder('utf-8');
-        // let done = false;
-        // while (!done) {
-        //     const { value, done: readerDone, ...others } = await reader.read();
-        //     console.log(value, others);
-        //     if (value) {
-        //         const char = decoder.decode(value);
-        //         console.log(char);
-        //         if (char === '\n' && answer().endsWith('\n')) {
-        //             continue;
-        //         }
-        //         if (char) {
-        //             setAnswer(answer() + char);
-        //         }
-        //     }
-        //     done = readerDone;
+        // const reader = data.getIterator();
+        // for await (const chunk of reader) {
+        //     // 解析数据块
+        //     console.log(JSON.stringify(chunk, null, 2));
         // }
+        const reader = data.getReader();
+        const decoder = new TextDecoder('utf-8');
+        let done = false;
+        while (!done) {
+            const answer = await reader.read();
+            console.log(answer)
+            // const { value, done: readerDone, ...others } = await reader.read();
+            // console.log(value, others);
+            // if (value) {
+            //     const char = decoder.decode(value);
+            //     console.log(char);
+            //     if (char === '\n' && answer().endsWith('\n')) {
+            //         continue;
+            //     }
+            //     if (char) {
+            //         setAnswer(answer() + char);
+            //     }
+            // }
+            // done = readerDone;
+        }
         // const result = await response.json();
         // if (result.error) {
         //     setAnswer(result.error.message);

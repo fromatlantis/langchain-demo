02cdafeb06052cf6460482d8623be462b452fbcd
niuzhiwei3 <niuzhiwei3@jd.com>
2024-01-15 15:52:09 +0800

    test: no stream

--- src/gpts/saler/service.ts
+++ src/gpts/saler/service.ts
@@ -21,37 +21,37 @@ export class Service {
             const chatHistory = this.chatHistory;
             chatHistory.push(new HumanMessage(input));
             console.log(chatHistory);
-            const result = this.executor.streamLog({
+            const result = await this.executor.invoke({
                 input,
                 chat_history: chatHistory,
             });
-            // this.chatHistory.push(new AIMessage(result.output));
-            // return result.output;
-            return new ReadableStream<Uint8Array>({
-                async start(controller) {
-                    const encoder = new TextEncoder();
-                    for await (const chunk of result) {
-                        // console.log(chunk)
-                        if (chunk.ops?.length > 0 && chunk.ops[0].op === 'add') {
-                            const addOp = chunk.ops[0];
-                            // console.log(addOp.path, addOp.value)
-                            if (
-                                addOp.path.startsWith('/logs/ChatOpenAI') &&
-                                typeof addOp.value === 'string' &&
-                                addOp.value.length
-                            ) {
-                                // console.log(addOp, chunk)
-                                const uint8Array = encoder.encode(addOp.value);
-                                controller.enqueue(uint8Array);
-                            } else if (addOp.path === '/streamed_output/-') {
-                                // console.log('all', addOp);
-                                chatHistory.push(new AIMessage(addOp.value.output));
-                            }
-                        }
-                    }
-                    controller.close();
-                },
-            });
+            this.chatHistory.push(new AIMessage(result.output));
+            return result.output;
+            // return new ReadableStream<Uint8Array>({
+            //     async start(controller) {
+            //         const encoder = new TextEncoder();
+            //         for await (const chunk of result) {
+            //             // console.log(chunk)
+            //             if (chunk.ops?.length > 0 && chunk.ops[0].op === 'add') {
+            //                 const addOp = chunk.ops[0];
+            //                 // console.log(addOp.path, addOp.value)
+            //                 if (
+            //                     addOp.path.startsWith('/logs/ChatOpenAI') &&
+            //                     typeof addOp.value === 'string' &&
+            //                     addOp.value.length
+            //                 ) {
+            //                     // console.log(addOp, chunk)
+            //                     const uint8Array = encoder.encode(addOp.value);
+            //                     controller.enqueue(uint8Array);
+            //                 } else if (addOp.path === '/streamed_output/-') {
+            //                     // console.log('all', addOp);
+            //                     chatHistory.push(new AIMessage(addOp.value.output));
+            //                 }
+            //             }
+            //         }
+            //         controller.close();
+            //     },
+            // });
         } else {
             return '代理加载中...';
         }

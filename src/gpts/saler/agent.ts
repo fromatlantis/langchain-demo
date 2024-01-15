6bedb183833e3e29c9a162d4c077447e8dfda187
niuzhiwei3 <niuzhiwei3@jd.com>
2024-01-15 15:47:58 +0800

    test: prompt

--- src/gpts/saler/agent.ts
+++ src/gpts/saler/agent.ts
@@ -28,7 +28,7 @@ export const genExecutor = async (openAIApiKey: string) => {
     const embeddings = new OpenAIEmbeddings({
         openAIApiKey,
     });
-    const tools = [await productSearch(model, embeddings)];
+    const tools = [await productSearch(model, embeddings), conversationStage(model)];
 
     const modelWithFunctions = model.bind({
         functions: tools.map((tool) => formatToOpenAIFunction(tool) as any),

de306cd4e9f0a589a5bc6b755a75a7b1369b2d95
niuzhiwei3 <niuzhiwei3@jd.com>
2024-01-25 10:29:33 +0800

    test: stream

--- src/gpts/saler/tools.ts
+++ src/gpts/saler/tools.ts
@@ -29,8 +29,8 @@ export async function productSearch(llm: BaseLanguageModel, embeddings: OpenAIEm
     const retriever = vectorstore.asRetriever();
     const chain = RetrievalQAChain.fromLLM(llm, retriever);
     return new ChainTool({
-        name: '产品搜索',
-        description: '当您需要回答有关床垫产品信息的问题时非常有用',
+        name: 'producs-search',
+        description: '当您需要回答有关床垫产品信息的问题时非常有用，请务必使用中文回复',
         chain,
     });
 }

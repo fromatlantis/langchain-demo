84340501838c7f685751f0297ec3bb096fd43cac
niuzhiwei3 <niuzhiwei3@jd.com>
2024-01-25 20:09:45 +0800

    test: tool

--- src/gpts/saler/agent.ts
+++ src/gpts/saler/agent.ts
@@ -63,7 +63,7 @@ export const genExecutor = async (openAIApiKey: string) => {
     ]);
     const prompt1 = new PromptTemplate({
         template: STAGE_ANALYZER_INCEPTION_PROMPT,
-        inputVariables: ['chat_history', 'conversation_stage_id'],
+        inputVariables: ['chat_history', 'conversation_stage'],
     });
     const chain = prompt1.pipe(model).pipe(new StringOutputParser());
     const runnableAgent = RunnableSequence.from([

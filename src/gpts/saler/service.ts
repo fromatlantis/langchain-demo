2e99d43ae4ddb094552310af3685ef93b96ba0ff
niuzhiwei3 <niuzhiwei3@jd.com>
2024-02-01 23:32:48 +0800

    test: output

--- src/gpts/saler/service.ts
+++ src/gpts/saler/service.ts
@@ -31,34 +31,21 @@ export class Service {
                 async start(controller) {
                     const encoder = new TextEncoder();
                     for await (const chunk of result) {
-                        console.log(chunk);
-                        if (chunk.ops?.length > 0) {
-                            const [firstOp] = chunk.ops;
+                        // console.log(chunk)
+                        if (chunk.ops?.length > 0 && chunk.ops[0].op === 'add') {
+                            const addOp = chunk.ops[0];
                             // console.log(addOp.path, addOp.value)
                             if (
-                                firstOp.op === 'add' &&
-                                firstOp.path.startsWith('/logs/ChatOpenAI') &&
-                                typeof firstOp.value === 'string' &&
-                                firstOp.value.length
+                                addOp.path.startsWith('/logs/ChatOpenAI') &&
+                                typeof addOp.value === 'string' &&
+                                addOp.value.length
                             ) {
                                 // console.log(addOp, chunk)
-                                const uint8Array = encoder.encode(firstOp.value);
+                                const uint8Array = encoder.encode(addOp.value);
                                 controller.enqueue(uint8Array);
-                            } else if (firstOp.path.endsWith('/final_output')) {
+                            } else if (addOp.path === '/streamed_output/-') {
                                 // console.log('all', addOp);
-                                if (
-                                    firstOp.op === 'replace' &&
-                                    typeof firstOp.value?.output === 'string'
-                                ) {
-                                    chatHistory.push(new AIMessage(firstOp.value.output));
-                                } else if (
-                                    firstOp.op === 'add' &&
-                                    typeof firstOp.value?.output === 'string'
-                                ) {
-                                    const uint8Array = encoder.encode(firstOp.value.output);
-                                    controller.enqueue(uint8Array);
-                                    chatHistory.push(new AIMessage(firstOp.value.output));
-                                }
+                                chatHistory.push(new AIMessage(addOp.value.output));
                             }
                         }
                     }

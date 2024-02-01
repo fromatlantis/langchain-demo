d97270f9c325016f24e839591c9f6ff48b47f10f
niuzhiwei3 <niuzhiwei3@jd.com>
2024-02-02 00:40:28 +0800

    test: output

--- src/gpts/saler/tools.ts
+++ src/gpts/saler/tools.ts
@@ -60,7 +60,7 @@ export const conversationStage = (llm: BaseLanguageModel, verbose: boolean = fal
 export async function get_working_hours(llm: BaseLanguageModel, embeddings: OpenAIEmbeddings) {
     const splitter = new CharacterTextSplitter({
         chunkSize: 100,
-        chunkOverlap: 10,
+        chunkOverlap: 0,
     });
     const docs = await splitter.createDocuments([works.content]);
     const new_docs = await splitter.splitDocuments(docs);

d6ea0ab72402612aedbc24b1984c1988c3fb1200
niuzhiwei3 <niuzhiwei3@jd.com>
2024-05-15 21:26:48 +0800

    test: jm

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,35 @@
-import { createSignal, For, Show } from 'solid-js';
+import { createSignal, onMount, For, Show } from 'solid-js';
 
 const Home = () => {
     const [key, setKey] = createSignal(localStorage.getItem('localKey') || '');
-
+    const [code, setCode] = createSignal('');
     const handleChange = (e: CustomEvent) => {
         setKey(e.detail.value);
         localStorage.setItem('localKey', e.detail.value);
     };
 
+    onMount(() => {
+        var param = {
+            appKey: 'ri1otwHDtOUnX6YmLjEg',
+            callback: (result: any) => {
+                console.log(result);
+                setCode(result.code);
+            },
+        };
+        // @ts-ignore
+        jme.login.getAuthorizationCode(param);
+    });
+
     return (
-        <ar-textarea
-            class="w-lg"
-            value={key()}
-            onChange={handleChange}
-            placeholder="OpenAI API Key"
-        ></ar-textarea>
+        <>
+            <span>{code()}</span>
+            <ar-textarea
+                class="w-lg"
+                value={key()}
+                onChange={handleChange}
+                placeholder="OpenAI API Key"
+            ></ar-textarea>
+        </>
     );
 };
 

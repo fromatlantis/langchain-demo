0d9e573192005d95692ba53e4e327affde0e640d
niuzhiwei3 <niuzhiwei3@jd.com>
2024-05-17 16:01:13 +0800

    test: jm usr

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,14 +3,14 @@ import { createSignal, createEffect, onMount, For, Show } from 'solid-js';
 const Home = () => {
     const [key, setKey] = createSignal(localStorage.getItem('localKey') || '');
     const [code, setCode] = createSignal('');
-    const [user, setUser] = createSignal('');
+    const [user, setUser] = createSignal<{ avatar: string; name: string }>();
     const handleChange = (e: CustomEvent) => {
         setKey(e.detail.value);
         localStorage.setItem('localKey', e.detail.value);
     };
     createEffect(async () => {
-        if(!code()) return;
-        alert(code())
+        if (!code()) return;
+        // alert(code())
         const response = await fetch(`/api/jmToken`, {
             headers: {
                 'Content-Type': 'application/json',
@@ -21,7 +21,7 @@ const Home = () => {
             }),
         });
         const res = await response.json();
-        alert(JSON.stringify(res));
+        // alert(JSON.stringify(res));
         if (!res.resultData.data.token) return;
         const responseUser = await fetch(`/api/jmUser`, {
             headers: {
@@ -33,8 +33,11 @@ const Home = () => {
             }),
         });
         const resUser = await responseUser.json();
-        setUser(resUser);
-        alert(JSON.stringify(resUser));
+        setUser({
+            avatar: resUser.resultData.data.avatarUrl,
+            name: resUser.resultData.data.name,
+        });
+        // alert(JSON.stringify(resUser));
     });
 
     onMount(() => {
@@ -51,7 +54,12 @@ const Home = () => {
 
     return (
         <>
-            <span>{code()}</span>
+            <p>code: {code()}</p>
+            <p>用户信息：</p>
+            <p class="flex gap-2 items-center">
+                <img class="w-10" src={user().avatar} alt="" />
+                <span>{user().name}</span>
+            </p>
             {user}
         </>
     );
